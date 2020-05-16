03223e17bb250a4c64721528d4f4a4d72aa188ca
zhangxwww <631133544@qq.com>
2020-05-17 00:26:12 +0800

    queryres

--- src/main/java/cn/edu/thssdb/query/QueryResult.java
+++ src/main/java/cn/edu/thssdb/query/QueryResult.java
@@ -8,18 +8,46 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import cn.edu.thssdb.schema.Table;
 import javafx.scene.control.Cell;
 
 public class QueryResult {
 
     private List<MetaInfo> metaInfoInfos;
+    // 查询哪些列，如果有多张表，提前计算join后的index
     private List<Integer> index;
     private List<Cell> attrs;
+    private boolean needJoin = false;
+    // join on 哪些列
+    private int joinIndex1, joinIndex2;
 
-    public QueryResult(QueryTable[] queryTables) {
+    public QueryResult(QueryTable[] queryTables, List<Integer> index, boolean needJoin, int joinIndex1, int joinIndex2) {
         // TODO
-        this.index = new ArrayList<>();
+        this.index = index;
+        this.needJoin = needJoin;
+        this.joinIndex1 = joinIndex1;
+        this.joinIndex2 = joinIndex2;
         this.attrs = new ArrayList<>();
+    }
+
+    public List<Row> join(List<Row> r1, List<Row> r2) {
+        int l1 = r1.size();
+        int l2 = r2.size();
+        List<Row> joinedRows = new ArrayList<>();
+        for (int i = 0; i < l1; ++i) {
+            for (int j = 0; j < l2; ++j) {
+                Row row1 = r1.get(i);
+                Row row2 = r2.get(j);
+                if (row1.getEntries().get(joinIndex1).equals(row2.getEntries().get(joinIndex2))) {
+                    LinkedList<Row> tmp = new LinkedList<Row>() {{
+                        add(row1);
+                        add(row2);
+                    }};
+                    joinedRows.add(combineRow(tmp));
+                }
+            }
+        }
+        return joinedRows;
     }
 
     public static Row combineRow(LinkedList<Row> rows) {
@@ -32,7 +60,10 @@ public class QueryResult {
     }
 
     public Row generateQueryRecord(Row row) {
-        // TODO
-        return null;
+        Entry[] entries = new Entry[index.size()];
+        for (int i = 0; i < index.size(); ++i) {
+            entries[i] = row.getEntries().get(index.get(i));
+        }
+        return new Row(entries);
     }
 }
