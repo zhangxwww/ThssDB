2422f1e2a64c063ddbbb40c9f4eca4e45512a90b
agil27 <wang-yb16@mails.tsinghua.edu.cn>
2020-05-16 23:35:20 +0800

    Query Table

--- src/main/java/cn/edu/thssdb/query/QueryTable.java
+++ src/main/java/cn/edu/thssdb/query/QueryTable.java
@@ -1,24 +1,75 @@
 package cn.edu.thssdb.query;
 
+import cn.edu.thssdb.schema.Column;
+import cn.edu.thssdb.schema.Entry;
 import cn.edu.thssdb.schema.Row;
+import cn.edu.thssdb.schema.Table;
+import org.w3c.dom.Attr;
+
 import java.util.Iterator;
+import java.util.List;
 
 public class QueryTable implements Iterator<Row> {
-  AttrCompare attrCompare;
-  QueryTable(AttrCompare attrCompare) {
-    // TODO
-    this.attrCompare = attrCompare;
-  }
+    private AttrCompare attrCompare;
+    private Iterator<Row> iterator;
+    private boolean isAttrInstCmp;
+    private int index1 = -1;
+    private int index2 = -1;
+    private Entry instant;
+    private Row nextRow;
 
-  @Override
-  public boolean hasNext() {
-    // TODO
-    return true;
-  }
+    QueryTable(AttrCompare attrCompare, Table table, String attr1, String attr2) {
+        // TODO
+        this.attrCompare = attrCompare;
+        this.iterator = table.iterator();
+        List<Column> columns = table.getColumns();
+        int numColumns = columns.size();
+        for (int i = 0; i < numColumns; i++) {
+            Column c = columns.get(i);
+            if (c.getName().equals(attr1)) {
+                this.index1 = i;
+            } else if (c.getName().equals(attr2)) {
+                this.index2 = i;
+            }
+        }
+        this.isAttrInstCmp = false;
+    }
 
-  @Override
-  public Row next() {
-    // TODO
-    return null;
-  }
+    QueryTable(AttrCompare attrCompare, Table table, String attr, Entry instant) {
+        this.attrCompare = attrCompare;
+        List<Column> columns = table.getColumns();
+        int numColumns = columns.size();
+        for (int i = 0; i < numColumns; i++) {
+            Column c = columns.get(i);
+            if (c.getName().equals(attr)) {
+                this.index1 = i;
+            }
+        }
+        this.instant = instant;
+        this.isAttrInstCmp = true;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (this.iterator.hasNext()) {
+            Row next = this.iterator.next();
+            Entry entry1 = next.getEntries().get(this.index1);
+            Entry entry2 = instant;
+            if (!this.isAttrInstCmp) {
+                entry2 = next.getEntries().get(this.index2);
+            }
+            if (this.attrCompare.eval(entry1, entry2)) {
+                this.nextRow = next;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Row next() {
+        Row result = this.nextRow;
+        this.nextRow = null;
+        return result;
+    }
 }
--- src/main/java/cn/edu/thssdb/schema/Table.java
+++ src/main/java/cn/edu/thssdb/schema/Table.java
@@ -12,181 +12,185 @@ import java.util.Iterator;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Table implements Iterable<Row> {
-  ReentrantReadWriteLock lock;
-  private String databaseName;
-  public String tableName;
-  public ArrayList<Column> columns;
-  public BPlusTree<Entry, Row> index;
-  public int primaryIndex;
+    ReentrantReadWriteLock lock;
+    private String databaseName;
+    public String tableName;
+    private ArrayList<Column> columns;
+    public BPlusTree<Entry, Row> index;
+    public int primaryIndex;
 
 
-  public Table(String databaseName, String tableName, Column[] columns) {
-    // TODO
-    this.lock = new ReentrantReadWriteLock();
-    this.databaseName = databaseName;
-    this.tableName = tableName;
-    this.columns = new ArrayList<>(Arrays.asList(columns));
-    this.index = new BPlusTree<>();
-    for (int i = 0; i < columns.length; ++i) {
-      if (columns[i].isPrimary()) {
-        this.primaryIndex = i;
-        break;
-      }
+    public Table(String databaseName, String tableName, Column[] columns) {
+        // TODO
+        this.lock = new ReentrantReadWriteLock();
+        this.databaseName = databaseName;
+        this.tableName = tableName;
+        this.columns = new ArrayList<>(Arrays.asList(columns));
+        this.index = new BPlusTree<>();
+        for (int i = 0; i < columns.length; ++i) {
+            if (columns[i].isPrimary()) {
+                this.primaryIndex = i;
+                break;
+            }
+        }
+
     }
 
-  }
-
-  public Table(String databaseName, String tableName, Column[] columns, String primaryName) {
-    // TODO
-    this.lock = new ReentrantReadWriteLock();
-    this.databaseName = databaseName;
-    this.tableName = tableName;
-    this.columns = new ArrayList<>(Arrays.asList(columns));
-    this.index = new BPlusTree<>();
-    for (int i = 0; i < columns.length; ++i) {
-      if (columns[i].getName().equals(primaryName)) {
-        this.primaryIndex = i;
-        break;
-      }
+    public Table(String databaseName, String tableName, Column[] columns, String primaryName) {
+        // TODO
+        this.lock = new ReentrantReadWriteLock();
+        this.databaseName = databaseName;
+        this.tableName = tableName;
+        this.columns = new ArrayList<>(Arrays.asList(columns));
+        this.index = new BPlusTree<>();
+        for (int i = 0; i < columns.length; ++i) {
+            if (columns[i].getName().equals(primaryName)) {
+                this.primaryIndex = i;
+                break;
+            }
+        }
     }
-  }
 
-  public Table(String databaseName, String tableName, Column[] columns, int primaryIndex) {
-    // TODO
-    this.lock = new ReentrantReadWriteLock();
-    this.databaseName = databaseName;
-    this.tableName = tableName;
-    this.columns = new ArrayList<>(Arrays.asList(columns));
-    this.index = new BPlusTree<>();
-    this.primaryIndex = primaryIndex;
-  }
-
-  public void recover(PageFilePersist persistManager) {
-    // TODO
-    //从磁盘中反序列化得到纪录
-    ArrayList<Row> rows = deserialize(persistManager);
-    for (Row row : rows){
-      Entry e = row.getEntries().get(primaryIndex);
-      index.put(e, row);
+    public Table(String databaseName, String tableName, Column[] columns, int primaryIndex) {
+        // TODO
+        this.lock = new ReentrantReadWriteLock();
+        this.databaseName = databaseName;
+        this.tableName = tableName;
+        this.columns = new ArrayList<>(Arrays.asList(columns));
+        this.index = new BPlusTree<>();
+        this.primaryIndex = primaryIndex;
     }
-  }
 
-  public void recover() {
-    // TODO
-  }
-
-  public void insert(Row row) {
-    // TODO
-    Entry e = row.getEntries().get(primaryIndex);
-    index.put(e, row);
-  }
-
-  public void delete(Row row) {
-    // TODO
-    Entry e = row.getEntries().get(primaryIndex);
-    index.remove(e);
-  }
-
-  public void delete(Iterator<Row> iterator) {
-    while (iterator.hasNext()) {
-      Row r = iterator.next();
-      delete(r);
+    public void recover(PageFilePersist persistManager) {
+        // TODO
+        //从磁盘中反序列化得到纪录
+        ArrayList<Row> rows = deserialize(persistManager);
+        for (Row row : rows) {
+            Entry e = row.getEntries().get(primaryIndex);
+            index.put(e, row);
+        }
     }
-  }
 
-  public void update(int attrIndex, Entry attrValue, Row row) {
-    // TODO
-    Entry e = row.getEntries().get(primaryIndex);
-    row.getEntries().set(attrIndex, attrValue);
-    index.update(e, row);
-  }
-
-  public void update(int attrIndex, Entry attrValue, Iterator<Row> rows) {
-    // TODO
-    for (; rows.hasNext(); ) {
-      Row r = rows.next();
-      update(attrIndex, attrValue, r);
+    public void recover() {
+        // TODO
     }
-  }
+
+    public void insert(Row row) {
+        // TODO
+        Entry e = row.getEntries().get(primaryIndex);
+        index.put(e, row);
+    }
+
+    public void delete(Row row) {
+        // TODO
+        Entry e = row.getEntries().get(primaryIndex);
+        index.remove(e);
+    }
+
+    public void delete(Iterator<Row> iterator) {
+        while (iterator.hasNext()) {
+            Row r = iterator.next();
+            delete(r);
+        }
+    }
+
+    public void update(int attrIndex, Entry attrValue, Row row) {
+        // TODO
+        Entry e = row.getEntries().get(primaryIndex);
+        row.getEntries().set(attrIndex, attrValue);
+        index.update(e, row);
+    }
+
+    public void update(int attrIndex, Entry attrValue, Iterator<Row> rows) {
+        // TODO
+        for (; rows.hasNext(); ) {
+            Row r = rows.next();
+            update(attrIndex, attrValue, r);
+        }
+    }
 
 
-//   When this function is called, those dirty pages that related to this table
+    //   When this function is called, those dirty pages that related to this table
 //   will be flushed to corresponding position on disks.
-  public void persist(PageFilePersist persistManager) {
-    // TODO
-    persistManager.flushTable(tableName);
+    public void persist(PageFilePersist persistManager) {
+        // TODO
+        persistManager.flushTable(tableName);
 
-  }
-
-  ////   Take the rows to the buffer pool
-  public void serialize(PageFilePersist persistManager) {
-    // TODO
-    try{
-      ByteArrayOutputStream bOutStream = new ByteArrayOutputStream();
-      ObjectOutputStream oos = new ObjectOutputStream(bOutStream);
-      Iterator<Row> iter = iterator();
-      while(iter.hasNext()){
-        Row r = iter.next();
-        oos.writeObject(r);
-      }
-      byte[] bData = bOutStream.toByteArray();
-      persistManager.storeTable(tableName,bData);
-
-    }catch (Exception e) {
-      e.printStackTrace();
     }
 
+    ////   Take the rows to the buffer pool
+    public void serialize(PageFilePersist persistManager) {
+        // TODO
+        try {
+            ByteArrayOutputStream bOutStream = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bOutStream);
+            Iterator<Row> iter = iterator();
+            while (iter.hasNext()) {
+                Row r = iter.next();
+                oos.writeObject(r);
+            }
+            byte[] bData = bOutStream.toByteArray();
+            persistManager.storeTable(tableName, bData);
 
-  }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-  private ArrayList<Row> deserialize(PageFilePersist persistManager) {
-    // TODO
-    ArrayList<Row> rows = new ArrayList<Row>();
-    byte[] bData = persistManager.retrieveTable(tableName);
-    index = new BPlusTree<Entry, Row>();
-    try{
-      ByteArrayInputStream bInStream = new ByteArrayInputStream(bData);
-      ObjectInputStream ois = new ObjectInputStream(bInStream);
-      while(true){
-        Row o = (Row) ois.readObject();
-        rows.add(o);
-      }
-    }catch (EOFException e) {
-      System.out.println("table deserialize中类对象已完全读入");
-    }catch (Exception e) {
-      e.printStackTrace();
+
     }
-    return rows;
-  }
 
-  public void printBPlusTree(){
-    Iterator<Row> iter = iterator();
-    while(iter.hasNext()){
-      Row r = iter.next();
-      System.out.println(r.toString());
+    private ArrayList<Row> deserialize(PageFilePersist persistManager) {
+        // TODO
+        ArrayList<Row> rows = new ArrayList<Row>();
+        byte[] bData = persistManager.retrieveTable(tableName);
+        index = new BPlusTree<Entry, Row>();
+        try {
+            ByteArrayInputStream bInStream = new ByteArrayInputStream(bData);
+            ObjectInputStream ois = new ObjectInputStream(bInStream);
+            while (true) {
+                Row o = (Row) ois.readObject();
+                rows.add(o);
+            }
+        } catch (EOFException e) {
+            System.out.println("table deserialize中类对象已完全读入");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rows;
     }
-  }
 
-  private class TableIterator implements Iterator<Row> {
-    private Iterator<Pair<Entry, Row>> iterator;
+    public void printBPlusTree() {
+        Iterator<Row> iter = iterator();
+        while (iter.hasNext()) {
+            Row r = iter.next();
+            System.out.println(r.toString());
+        }
+    }
 
-    TableIterator(Table table) {
-      this.iterator = table.index.iterator();
+    public ArrayList<Column> getColumns() {
+        return this.columns;
+    }
+
+    private class TableIterator implements Iterator<Row> {
+        private Iterator<Pair<Entry, Row>> iterator;
+
+        TableIterator(Table table) {
+            this.iterator = table.index.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Row next() {
+            return iterator.next().getValue();
+        }
     }
 
     @Override
-    public boolean hasNext() {
-      return iterator.hasNext();
+    public Iterator<Row> iterator() {
+        return new TableIterator(this);
     }
-
-    @Override
-    public Row next() {
-      return iterator.next().getValue();
-    }
-  }
-
-  @Override
-  public Iterator<Row> iterator() {
-    return new TableIterator(this);
-  }
 }
