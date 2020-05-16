e02bad9bc184efe71748c56ad32c7ee45c9d8da9
zhangxwww <631133544@qq.com>
2020-05-16 23:50:18 +0800

    queryres

--- src/main/java/cn/edu/thssdb/query/QueryResult.java
+++ src/main/java/cn/edu/thssdb/query/QueryResult.java
@@ -1,30 +1,38 @@
 package cn.edu.thssdb.query;
 
+import cn.edu.thssdb.schema.Entry;
 import cn.edu.thssdb.schema.Row;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
 import javafx.scene.control.Cell;
 
 public class QueryResult {
 
-  private List<MetaInfo> metaInfoInfos;
-  private List<Integer> index;
-  private List<Cell> attrs;
+    private List<MetaInfo> metaInfoInfos;
+    private List<Integer> index;
+    private List<Cell> attrs;
 
-  public QueryResult(QueryTable[] queryTables) {
-    // TODO
-    this.index = new ArrayList<>();
-    this.attrs = new ArrayList<>();
-  }
+    public QueryResult(QueryTable[] queryTables) {
+        // TODO
+        this.index = new ArrayList<>();
+        this.attrs = new ArrayList<>();
+    }
 
-  public static Row combineRow(LinkedList<Row> rows) {
-    // TODO
-    return null;
-  }
+    public static Row combineRow(LinkedList<Row> rows) {
+        // TODO
+        ArrayList<Entry> entries = new ArrayList<>();
+        for (Row r : rows) {
+            entries.addAll(r.getEntries());
+        }
+        return new Row((Entry[]) entries.toArray());
+    }
 
-  public Row generateQueryRecord(Row row) {
-    // TODO
-    return null;
-  }
+    public Row generateQueryRecord(Row row) {
+        // TODO
+        return null;
+    }
 }
